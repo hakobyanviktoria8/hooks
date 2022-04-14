72c2a070e103d87c78740a2376ecee1f04b64040
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-14 11:54:37 +0400

    useState hook

--- src/hooks/UseState.js
+++ src/hooks/UseState.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react'
 
 function UseState() {
     const[count, setCount] = useState(0)
+
+    const[show, setShow] = useState(true)
   return (
     <div>
         <h1>UseState Hook</h1>
         <p>{count}</p>
-        <button onClick={()=>setCount(count+1)}>+</button>
+        <button onClick={()=>{
+            setCount(count+1)
+            setShow(!show)
+        }}>+</button>
         <button onClick={()=>setCount(count-1)}>-</button>
+        <hr/>
+        {show && <p>Show some text</p>}
     </div>
   )
 }
